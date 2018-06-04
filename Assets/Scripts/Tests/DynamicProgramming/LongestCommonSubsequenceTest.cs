f373b183629f0b52169d94b47b5add834bd93c26
unknown <szymon.brych@gmail.com>
2018-06-04 23:07:08 +0100

    bubble sort

--- Assets/Scripts/Tests/DynamicProgramming/LongestCommonSubsequenceTest.cs
+++ Assets/Scripts/Tests/DynamicProgramming/LongestCommonSubsequenceTest.cs
@@ -1,23 +1,26 @@
 using NUnit.Framework;
 using System.Collections.Generic;
 
-public class LongestCommonSubsequenceTest
+namespace DynamicProgramming
 {
+    public class LongestCommonSubsequenceTest
+    {
 
-	[SetUp]
-	public void SetUp()
-	{
-	}
+        [SetUp]
+        public void SetUp()
+        {
+        }
 
-	[TearDown]
-	public void TearDown()
-	{
-	}
+        [TearDown]
+        public void TearDown()
+        {
+        }
 
-	[Test]
-	public void t()
-	{
-        Assert.AreEqual(new List<int> { 2, 4, 6 }, LongestCommonSubsequence.GetLongestSubsequence(new List<int>{1,2,3,4,5,6}, new List<int>{2,4,6,8,10}));
-	}
+        [Test]
+        public void t()
+        {
+            Assert.AreEqual(new List<int> { 2, 4, 6 }, LongestCommonSubsequence.GetLongestSubsequence(new List<int> { 1, 2, 3, 4, 5, 6 }, new List<int> { 2, 4, 6, 8, 10 }));
+        }
 
+    }
 }
